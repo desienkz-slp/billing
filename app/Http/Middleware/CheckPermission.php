6a0a6e4273035d3056e566b46fac5d3f1a4d2030
Desienkz SLP <superadmin@radius-ui.local>
2026-06-27 01:41:01 +0700

    Fix permission middleware mapping for isolir/cuti

--- app/Http/Middleware/CheckPermission.php
+++ app/Http/Middleware/CheckPermission.php
@@ -1,8 +1,8 @@
 <?php
 /*   __________________________________________________
     |  Obfuscated by YAK Pro - Php Obfuscator  3.0.0   |
-    |              on 2026-06-25 10:49:51              |
+    |              on 2026-06-26 20:40:51              |
     |    GitHub: https://github.com/pk-fr/yakpro-po    |
     |__________________________________________________|
 */
- namespace App\Http\Middleware; use Closure; use Illuminate\Http\Request; use Symfony\Component\HttpFoundation\Response; class CheckPermission { public function handle(Request $u9qhv, Closure $N_zj8, string ...$IZGZZ): Response { goto e4F06; i2Gek: if (!$Ie2cF) { abort(403, 'Akses ditolak.'); } goto QMcI2; FCXAB: $NQd6b = $Ie2cF->role; goto WRsMn; ypT6r: abort(403, 'Anda tidak memiliki izin untuk mengakses halaman ini.'); goto M53v4; QMcI2: if ($Ie2cF->is_system_admin) { return $N_zj8($u9qhv); } goto FCXAB; oknoF: foreach ($IZGZZ as $VS3ZQ) { $KL5tn = trim($VS3ZQ); if ($NQd6b->hasPermission($KL5tn)) { return $N_zj8($u9qhv); } } goto ypT6r; WRsMn: if (!$NQd6b) { abort(403, 'Role tidak ditemukan. Hubungi administrator.'); } goto oknoF; e4F06: $Ie2cF = $u9qhv->user(); goto i2Gek; M53v4: } }
+ namespace App\Http\Middleware; use Closure; use Illuminate\Http\Request; use Symfony\Component\HttpFoundation\Response; class CheckPermission { public function handle(Request $u9qhv, Closure $N_zj8, string ...$IZGZZ): Response { goto JusGH; sqKDm: foreach ($IZGZZ as $VS3ZQ) { $KL5tn = trim($VS3ZQ); if (str_contains($KL5tn, '.')) { if ($Ie2cF->hasCapability($KL5tn)) { return $N_zj8($u9qhv); } } else if ($NQd6b->hasPermission($KL5tn)) { return $N_zj8($u9qhv); } } goto s57t2; s57t2: abort(403, 'Anda tidak memiliki izin untuk mengakses halaman ini.'); goto qf2P_; Ubfk4: $NQd6b = $Ie2cF->role; goto xKdBw; ToU7B: if ($Ie2cF->is_system_admin) { return $N_zj8($u9qhv); } goto Ubfk4; TS3pi: if (!$Ie2cF) { abort(403, 'Akses ditolak.'); } goto ToU7B; xKdBw: if (!$NQd6b) { abort(403, 'Role tidak ditemukan. Hubungi administrator.'); } goto sqKDm; JusGH: $Ie2cF = $u9qhv->user(); goto TS3pi; qf2P_: } }
